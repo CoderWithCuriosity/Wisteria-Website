48c697fd2656e5803a7f559c9a442ef2a7196c81
Angry-Creator <nwankwochidera252565@gmail.com>
2022-10-14 23:32:39 +0100

    Just finished working on the Intersection with viewport

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
-// window.addEventListener("DOMContentLoaded", ()=>{
-//     if(window.innerWidth > )
-// })
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el)=>{
+    observer.observe(el)
+});
+
 window.addEventListener('scroll', ()=>{
     if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
         document.getElementById('navbar').classList.add('scrolled');
